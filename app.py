7ca46e09ad02b0a66d7f02e98a7feebebd659454
john tolulope afariogun <afariogunjohn2502@gmail.com>
2025-01-29 16:56:00 +0100

    initial

---
+++ app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from datetime import datetime
+from pytz import timezone
+from fastapi.middleware.cors import CORSMiddleware
+app=FastAPI()
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+        allow_credentials=True,
+    )
+@app.get('/')
+async def get_info():
+    current_datetime = datetime.now(timezone('UTC')).isoformat()
+
+    return {
+        "email": "afariogunjohn2502@gmail.com",
+        "current_datetime": current_datetime,
+        "github_url": "https://github.com/johnafariogun/HNG12/simple-api"
+    }
